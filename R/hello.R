833f3b6ffc405dd2a7fd5758558a1426261f0751
paul.blanc733@gmail.com <paul.blanc733@gmail.com>
2026-09-22 16:49:36 +0200

    init

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+#' Say hello
+#'
+#' Small test function.
+#'
+#' @param name Character string.
+#'
+#' @return Character string.
+#'
+#' @export
+hello <- function(name) {
+
+  paste("Hello", name)
+
+}
