dbb664180664c2a6d1fb32dd0ae6e3808df8b722
paul.blanc733@gmail.com <paul.blanc733@gmail.com>
2026-09-22 16:58:49 +0200

    karras

---
+++ R/MelSign_df_Karras.R
@@ -0,0 +1,11 @@
+#' MelSign gene signature
+#'
+#' Dataset containing the MelSign signature from Karras et al.
+#'
+#' @format A data frame with 198 rows and 2 variables:
+#' \describe{
+#'   \item{gene}{Gene symbol}
+#' }
+#'
+#' @source Karras et al.
+"MelSign_df_Karras"
